b703074a72c76996088a9b5a0b4fbb15c9eec517
mikarub <m.lennartsson@gmail.com>
2025-11-05 09:53:14 +0100

    New updated code, still not perfect

--- mygame.py
+++ mygame.py
@@ -8,23 +8,50 @@ import itertools
 import time
 import sys
 
-def spinner(stop_event):
+# Detect keypresses cross-platform
+try:
+	import msvcrt # in case anyone on windows ever use this code
+	def key_pressed():
+		return msvcrt.kbhit()
+except ImportError:
+	import select, termios, tty # Unix
+	import sys
+	def key_pressed():
+		dr, dw, de = select.select([sys.stdin], [], [], 0)
+		return bool(dr)
+
+def spinner(stop_event, pause_event):
+	"""
+	This is the actual spinner.
+	"""
 	spinner_cycle = itertools.cycle(['|', '/', '-', '\\'])
 	while not stop_event.is_set():
-		sys.stdout.write('\rWaiting for input...' + next(spinner_cycle))
-		sys.stdout.flush()
+		if not pause_event.is_set():
+			sys.stdout.write('\rWaiting for input...' + next(spinner_cycle))
+			sys.stdout.flush()
 		time.sleep(0.1)
-	sys.stdout.write('\r') # clear line when done
+	sys.stdout.write('\r' + ' ' * 40 + '\r') # clear line
 	
-# Create a stop event for the spinner
 stop_event = threading.Event()
-spinner_thread = threading.Thread(target=spinner, args=(stop_event,))
+pause_event = threading.Event()
+spinner_thread = threading.Thread(target=spinner, args=(stop_event, pause_event))
 
 # Start the spinner
 spinner_thread.start()
 
-# Wait for user input
-user_input = input(" Please type something: ")
+# Check for typing while waiting for input
+user_input = ''
+sys.stdout.write("Please type something: ")
+sys.stdout.flush()
+
+while True:
+	if key_pressed():
+		pause_event.set() # Pause spinner
+	try:
+		user_input = input() # Standard input
+		break
+	except KeyboardInterrupt:
+		break
 
 # Stop the spinner
 stop_event.set()
